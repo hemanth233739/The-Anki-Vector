81e0e3254b78b64411cae75bb422b815af021634
Deepak Jack <93731368+DeepakJack007@users.noreply.github.com>
2021-11-29 18:24:29 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -233,7 +233,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_photo(
-                      , 
+             AIGER_IMG, 
             "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
                 uptime
             ),
